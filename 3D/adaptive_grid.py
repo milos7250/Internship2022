31f893dc9f491ff4bdee9e628b41914dc5f2ed11
milosmicik <milos.micik7@gmail.com>
2022-06-23 12:53:49 +0100

    DAY 19 - Adaptive grids and merging triangular meshes
    I continued with creating a method to merge two contour triangular meshes created in two grids of different sizes. I abandoned using the glue layer and focused on altering the mesh points and the boundary to match correctly. The method works quite well. It could however be improved by removing unnecessary faces from the higher resolution mesh in order to avoid texture clipping.

--- 3D/adaptive_grid.py
+++ 3D/adaptive_grid.py
@@ -7,10 +7,12 @@ import numpy as np
 from scipy.ndimage import gaussian_filter, zoom
 from functions_3D import remove_duplicate_vertices, linear
 
-res = 11
-n = 4
-low_res = np.zeros([res] * 3, dtype=bool)
-high_res = np.zeros([((res - 1) * n + 1)] * 3, dtype=bool)
+res = 7
+n = 8
+a, b, c = res - 1, res / 2, res / 3
+a = b = c = res - 1
+low_res = np.zeros([res] * 3, dtype=float)
+high_res = np.zeros([((res - 1) * n + 1)] * 3, dtype=float)
 
 
 def coords_from_indices(x_idx, y_idx, z_idx, shape):
@@ -23,29 +25,28 @@ def coords_from_indices(x_idx, y_idx, z_idx, shape):
     ).T
 
 
+# indices = (slice(2, 6), slice(2, 6), slice(2, 5))
+# low_res[indices] = high_res[indices] = True
+
 for i, j, k in itertools.product(*[np.arange(shape) for shape in low_res.shape]):
     x, y, z = coords_from_indices(i, j, k, low_res.shape)
-    if x**2 + y**2 + z**2 <= (res - 1) ** 2:
-        low_res[i, j, k] = True
+    if (x / a) ** 2 + (y / b) ** 2 + (z / c) ** 2 <= 1:
+        low_res[i, j, k] = 1
 
 for i, j, k in itertools.product(*[np.arange(shape) for shape in high_res.shape]):
     x, y, z = coords_from_indices(i, j, k, high_res.shape)
-    if x**2 + y**2 + z**2 <= (res - 1) ** 2:
-        high_res[i, j, k] = True
+    if (x / a) ** 2 + (y / b) ** 2 + (z / c) ** 2 <= 1:
+        high_res[i, j, k] = 1
 
-x, y, z = np.indices(low_res.shape)
-X, Y, Z = np.indices(high_res.shape)
+
+x, y, z = coords_from_indices(*np.indices(low_res.shape), low_res.shape).T
+X, Y, Z = coords_from_indices(*np.indices(high_res.shape), high_res.shape).T
 
 
 # Apply gaussian filter and marching squares after merging
 # low_res_scaled = low_res.repeat(n, axis=0).repeat(n, axis=1).repeat(n, axis=2)[: -n + 1, : -n + 1, : -n + 1]
 low_res_scaled = zoom(
-    low_res,
-    high_res.shape[0] / low_res.shape[0],
-    order=0,
-    mode="nearest",
-    prefilter=False,
-    grid_mode=True,
+    low_res, high_res.shape[0] / low_res.shape[0], order=0, mode="nearest", prefilter=False, grid_mode=True
 )
 low_res_cut = low_res_scaled[:, :, : high_res.shape[0] // 2]
 high_res_cut = high_res[:, :, high_res.shape[0] // 2 :]
@@ -55,6 +56,124 @@ merged = gaussian_filter(merged - 0.5, 1) + 0.5
 vertices, faces, _, _ = marching_cubes(merged)
 vertices = coords_from_indices(*vertices.T, merged.shape)
 mlab.triangular_mesh(
+    vertices[:, 0],
+    vertices[:, 1],
+    vertices[:, 2],
+    faces,
+)
+mlab.points3d(
+    X,
+    Y,
+    Z + res * 2,
+    merged,
+    mode="cube",
+    scale_factor=1,
+    colormap="coolwarm",
+)
+
+
+# Apply gaussian filter and marching squares before merging
+
+
+# glue_layer_low = low_res[:, :, low_res.shape[0] // 2]
+# glue_layer_high = high_res[:, :, high_res.shape[0] // 2]
+# glue_layer_high = zoom(  # Interpolates the layer to higher resolution linearly
+#     glue_layer_high,
+#     low_res.shape[0] / high_res.shape[0],
+#     order=1,
+#     mode="nearest",
+#     prefilter=False,
+#     grid_mode=True,
+# )
+# glue_layer_low = (glue_layer_low + glue_layer_high) / 2
+# glue_layer_high = zoom(  # Interpolates the layer to higher resolution linearly
+#     glue_layer_low,
+#     high_res.shape[0] / low_res.shape[0],
+#     order=1,
+#     mode="nearest",
+#     prefilter=False,
+#     grid_mode=True,
+# )
+# low_res[:, :, low_res.shape[0] // 2] = glue_layer_low
+# high_res[:, :, high_res.shape[0] // 2] = glue_layer_high
+
+
+# low_res = gaussian_filter(low_res - 0.5, 1) + 0.5
+# high_res = gaussian_filter(high_res - 0.5, 1) + 0.5
+
+# import matplotlib.pyplot as plt
+#
+# plt.subplot(221)
+# plt.imshow(low_res[:, :, low_res.shape[0] // 2])
+# plt.subplot(222)
+# plt.imshow(high_res[:, :, high_res.shape[0] // 2])
+# plt.subplot(223)
+# plt.imshow(low_res[:, :, low_res.shape[0] // 2] >= 0.5)
+# plt.subplot(224)
+# plt.imshow(high_res[:, :, high_res.shape[0] // 2] >= 0.5)
+#
+# plt.show()
+
+
+def merge_meshes(vertices_hr, faces_hr, vertices_lr, faces_lr, x_range, y_range, z_range):
+    # Removing vertices from vertices 1!
+    mask_hr1 = np.all([x_range[0], y_range[0], z_range[0]] <= vertices_hr, axis=1)
+    mask_hr2 = np.all(vertices_hr <= [x_range[1], y_range[1], z_range[1]], axis=1)
+    mask_lr1 = np.all([x_range[0], y_range[0], z_range[0]] <= vertices_lr, axis=1)
+    mask_lr2 = np.all(vertices_lr <= [x_range[1], y_range[1], z_range[1]], axis=1)
+    mask_hr = np.logical_and(mask_hr1, mask_hr2)
+    mask_lr = np.logical_and(mask_lr1, mask_lr2)
+
+    high_to_low_pairing = np.argmin(
+        np.linalg.norm(vertices_hr[mask_hr, np.newaxis] - vertices_lr[mask_lr], axis=2), axis=1
+    )
+    pts_remove = np.nonzero(mask_hr)[0]
+    pts_keep = np.nonzero(mask_lr)[0][high_to_low_pairing]
+
+    for p_remove, p_keep in zip(pts_remove, pts_keep):
+        faces_hr.flat[faces_hr.flat == p_remove] = p_keep + vertices_hr.shape[0]
+
+    # TODO: Do this without loop
+    for idx in range(np.count_nonzero(mask_lr)):
+        low_to_high_pairing = high_to_low_pairing == idx
+        print(idx)
+        print(vertices_lr[mask_lr][idx])
+        print(vertices_hr[mask_hr][low_to_high_pairing])
+        vertices_lr[np.nonzero(mask_lr)[0][idx]] = (
+            vertices_lr[mask_lr][idx] + np.sum(vertices_hr[mask_hr][low_to_high_pairing], axis=0)
+        ) / (np.count_nonzero(low_to_high_pairing) + 1)
+        print(vertices_lr[mask_lr][idx])
+
+    # Remove repeated points from vertices1 and adjust face numbers accordingly
+    for idx in reversed(pts_remove):
+        faces_hr.flat[faces_hr.flat > idx] -= 1
+    vertices_hr = np.delete(vertices_hr, pts_remove, axis=0)
+
+    # Remove degenerate faces
+    delete = []
+    for idx, face in enumerate(faces_hr):
+        if np.unique(face).size != faces_hr.shape[1]:
+            delete.append(idx)
+    faces_hr = np.delete(faces_hr, delete, axis=0)
+    delete = []
+    for idx, face in enumerate(faces_lr):
+        if np.unique(face).size != faces_lr.shape[1]:
+            delete.append(idx)
+    faces_lr = np.delete(faces_lr, delete, axis=0)
+
+    vertices, faces = np.vstack([vertices_hr, vertices_lr]), np.vstack([faces_hr, faces_lr + vertices_hr.shape[0]])
+
+    return vertices, faces
+
+
+vertices_low, faces_low, _, _ = marching_cubes(low_res[:, :, : low_res.shape[0] // 2 + 1], level=0.5)
+vertices_low = coords_from_indices(*vertices_low.T, low_res.shape)
+vertices_high, faces_high, _, _ = marching_cubes(high_res[:, :, high_res.shape[0] // 2 :], level=0.5)
+vertices_high = coords_from_indices(*(vertices_high + [0, 0, high_res.shape[0] // 2]).T, high_res.shape)
+
+vertices, faces = np.vstack([vertices_high, vertices_low]), np.vstack([faces_high, faces_low + vertices_high.shape[0]])
+
+mlab.triangular_mesh(
     vertices[:, 0] + res * 2,
     vertices[:, 1],
     vertices[:, 2],
@@ -62,34 +181,32 @@ mlab.triangular_mesh(
 )
 
 
-# Apply gaussian filter and marching squares before merging
-low_res = gaussian_filter(low_res - 0.5, 1) + 0.5
-high_res = gaussian_filter(high_res - 0.5, 1) + 0.5
-
-glue_layer = low_res[:, :, low_res.shape[0] // 2]
-glue_layer = zoom(  # Interpolates the layer to higher resolution linearly
-    glue_layer.astype(float),
-    high_res.shape[0] / low_res.shape[0],
-    order=1,
-    mode="nearest",
-    prefilter=False,
-    grid_mode=True,
-)
-high_res[:, :, high_res.shape[0] // 2] = glue_layer
-
-
-vertices_low, faces_low, _, _ = marching_cubes(low_res[:, :, : low_res.shape[0] // 2 + 1], level=0.5)
-vertices_low = coords_from_indices(*vertices_low.T, low_res.shape)
-vertices_high, faces_high, _, _ = marching_cubes(high_res[:, :, high_res.shape[0] // 2 :], level=0.5)
-vertices_high = coords_from_indices(*(vertices_high + [0, 0, high_res.shape[0] // 2]).T, high_res.shape)
-vertices, faces = np.vstack([vertices_low, vertices_high]), np.vstack([faces_low, faces_high + vertices_low.shape[0]])
+vertices, faces = merge_meshes(vertices_high, faces_high, vertices_low, faces_low, [-res, res], [-res, res], [0, 0])
 
 mlab.triangular_mesh(
-    vertices[:, 0],
+    vertices[:, 0] + res * 4,
     vertices[:, 1],
     vertices[:, 2],
     faces,
 )
 
+mlab.points3d(
+    x[:, :, : low_res.shape[0] // 2 + 1] + res * 2,
+    y[:, :, : low_res.shape[0] // 2 + 1],
+    z[:, :, : low_res.shape[0] // 2 + 1] + res * 2,
+    low_res[:, :, : low_res.shape[0] // 2 + 1],
+    mode="cube",
+    scale_factor=1,
+    colormap="coolwarm",
+)
+mlab.points3d(
+    X[:, :, high_res.shape[0] // 2 :] + res * 2,
+    Y[:, :, high_res.shape[0] // 2 :],
+    Z[:, :, high_res.shape[0] // 2 :] + res * 2,
+    high_res[:, :, high_res.shape[0] // 2 :],
+    mode="cube",
+    scale_factor=1,
+    colormap="coolwarm",
+)
 
 mlab.show()
